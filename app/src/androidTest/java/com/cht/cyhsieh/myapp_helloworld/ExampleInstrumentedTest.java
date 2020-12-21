d5084cf32e3f9fc9e4cdc75adb02bd2a2b2982e0
cyhsieh1108 <cyhsieh1108@cht.com.tw>
2020-12-21 10:57:13 +0800

    unit test

--- app/src/androidTest/java/com/cht/cyhsieh/myapp_helloworld/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cht/cyhsieh/myapp_helloworld/ExampleInstrumentedTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.After;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +20,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.cht.cyhsieh.myapp_helloworld", appContext.getPackageName());
+
+        reportHelper.label("myTestStepLabel");
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 }
